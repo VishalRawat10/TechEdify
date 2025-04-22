c11c39bf1be98f6db1eaac55070eb41662f27294
Vishal Rawat <omniverserawat2005@gmail.com>
2025-04-22 15:17:36 +0530

    Added courses middlewares

---
+++ server/middlewares/courses.middleware.js
@@ -0,0 +1,31 @@
+const Lecture = require("../models/lecture");
+const Instructor = require("../models/instructor");
+const Course = require("../models/course");
+
+module.exports.isEnrolledStudent = async (req, res, next) => {
+    const { courseId } = req.params;
+    const course = await Course.findById(courseId).populate("lectures");
+    if (!course) {
+        return res.status(400).json({ message: "Course not found!", success: false });
+    }
+    if (req.user.coursesEnrolled.includes(courseId)) {
+        req.course = course;
+        return next();
+    }
+    return res.status(401).json({ message: "Unathorized!", success: true, error: "not logged in" });
+}
+
+module.exports.isCourseInstructor = async (req, res, next) => {
+    const { courseId } = req.params;
+    const course = await Course.findById(courseId).populate("instructor").populate("lectures");
+    if (!course) {
+        return res.status(400).json({ message: "Course not found!", success: false });
+    }
+
+    if (course.instructor.userId.toString() === req.user._id.toString()) {
+        req.course = course;
+        return next();
+    }
+
+    return res.status(401).json({ message: "Unathorized!", success: false });
+}
