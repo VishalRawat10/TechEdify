e9e7bfd28ebb33b51f27635021e69bbf8962b3f0
Vishal Rawat <omniverserawat2005@gmail.com>
2025-04-22 16:05:00 +0530

    Some logic changes

--- server/controllers/courses.controller.js
+++ server/controllers/courses.controller.js
@@ -1,11 +1,13 @@
 const Course = require("../models/course");
 const User = require("../models/user");
+const Review = require("../models/review");
+const Instructor = require("../models/instructor");
 
 module.exports.courses = async (req, res, next) => {
     try {
-        const courses = await Course.find();
+        const courses = await Course.find().populate("instructor");
         if (courses)
-            return res.status(200).json(courses);
+            return res.status(200).json({ courses });
         else
             res.status(500).json({ message: "Internal server error!" });
     } catch (err) {
@@ -17,18 +19,25 @@ module.exports.courses = async (req, res, next) => {
 module.exports.getCourse = async (req, res, next) => {
     const { id } = req.params;
     try {
-        const course = await Course.findById(id);
+        const course = await Course.findById(id).populate({
+            path: "reviews", populate: {
+                path: "author",
+                select: "fullname"
+            }
+        }).populate("instructor");
         if (course) {
-            return res.status(200).json(course);
+            return res.status(200).json({ course, message: "Course fetched succesfully." });
         } else {
             return res.status(400).json({ message: "Course not found." });
         }
     } catch (err) {
+        console.log(err);
         return res.status(500).json({ message: err.message });
     }
 }
 
-module.exports.buy = async (req, res, next) => {
+//enroll
+module.exports.enroll = async (req, res, next) => {
     const { id } = req.params;
     const course = await Course.findById(id);
     const user = await User.findById(req.user._id);
@@ -38,7 +47,7 @@ module.exports.buy = async (req, res, next) => {
     try {
         user.coursesEnrolled.push(course);
         await user.save();
-        res.status(200).json({ message: "Course purchased successfully!" });
+        res.status(200).json({ message: "Enrolled to the course successfully!" });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Internal server error!" });
