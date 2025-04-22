a5e2599675f13f6102a2ed7c465c777d8e21849d
Vishal Rawat <omniverserawat2005@gmail.com>
2025-04-22 15:33:50 +0530

    Added multiple routes and ChatBot btn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 const Home = lazy(() => import("./pages/Home"));
@@ -9,7 +9,10 @@ const Courses = lazy(() => import("./pages/Courses"));
 const ShowCourse = lazy(() => import("./pages/ShowCourse"));
 const Login = lazy(() => import("./pages/Login"));
 const Signup = lazy(() => import("./pages/Signup"));
-const UserProfile = lazy(() => import("./pages/UserProfile"));
+const UpdateProfile = lazy(() => import("./pages/UpdateProfile"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Lectures = lazy(() => import("./pages/Lectures"));
+const ChatBotPage = lazy(() => import("./pages/ChatBotPage"));
 
 import Loader from "./components/Loader";
 import Header from "./layouts/Header";
@@ -17,6 +20,7 @@ import Footer from "./layouts/Footer";
 
 import AuthWrapper from "./components/AuthWrapper";
 import AllContextProvider from "./context/AllContextProvider";
+import ChatWithAiBtn from "./components/UI/ChatWithAiBtn";
 
 export default function App() {
   return (
@@ -27,20 +31,39 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/courses/:id" element={<ShowCourse />} />
+          <Route
+            path="/courses/:id/learn"
+            element={
+              <AuthWrapper>
+                <Lectures />
+              </AuthWrapper>
+            }
+          />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/user/login" element={<Login />} />
           <Route path="/user/signup" element={<Signup />} />
           <Route
-            path="/user/profile"
+            path="/user/dashboard"
             element={
               <AuthWrapper>
-                <UserProfile />
+                <Dashboard />
               </AuthWrapper>
             }
           />
+          <Route
+            path="/user/update-profile"
+            element={
+              <AuthWrapper>
+                <UpdateProfile />
+              </AuthWrapper>
+            }
+          />
+          <Route path="/chatbot/chat" element={<ChatBotPage />} />
         </Routes>
       </Suspense>
+      <ChatWithAiBtn />
+
       <Footer />
     </AllContextProvider>
   );
