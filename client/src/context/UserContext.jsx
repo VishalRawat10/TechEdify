f1b4a91a8522a12734e513518b6f9214c80e878b
Vishal Rawat <omniverserawat2005@gmail.com>
2025-04-22 15:42:00 +0530

    some changes :

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,22 +1,28 @@
 import { createContext, useEffect, useState } from "react";
 import { apiInstance } from "../../services/apis";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState();
+  const navigate = useNavigate();
+
+  const getUser = async () => {
+    try {
+      const res = await apiInstance.get("/user/profile", {
+        withCredentials: true,
+      });
+      setUser(res.data.user);
+      navigate("/user/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
-    const getUser = async () => {
-      try {
-        const res = await apiInstance.get("/user/profile", {
-          withCredentials: true,
-        });
-        setUser(res.data.user);
-      } catch (err) {
-        console.log(err);
-      }
-    };
     getUser();
   }, []);
+  console.log(user);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
