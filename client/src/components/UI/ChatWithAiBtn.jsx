29e34f9b9f6825ce09c089494636adef9d033007
Vishal Rawat <omniverserawat2005@gmail.com>
2025-06-11 20:29:00 +0530

    Changed the route

--- client/src/components/UI/ChatWithAiBtn.jsx
+++ client/src/components/UI/ChatWithAiBtn.jsx
@@ -2,10 +2,14 @@ import { useLocation, Link } from "react-router-dom";
 
 export default function ChatWithAiBtn() {
   const location = useLocation();
-  if (location.pathname === "/chatbot/chat") return "";
+  if (
+    location.pathname === "/ai-mentor/chat" ||
+    location.pathname.includes("/user/instructor")
+  )
+    return "";
   return (
     <button className="cursor-pointer fixed bottom-6 right-6 w-fit px-8 py-4 bg-[#f8611b] rounded-full opacity-90 hover:opacity-100">
-      <Link to={"/chatbot/chat"} className="flex gap-2 font-semibold">
+      <Link to={"/ai-mentor/chat"} className="flex gap-2 font-semibold">
         Ask AI
         <img src="/images/chat.png" className="h-6" />
       </Link>
