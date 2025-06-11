47d8feeaae74528d9345fa43c4a13a7508e608db
Vishal Rawat <omniverserawat2005@gmail.com>
2025-06-11 15:17:31 +0530

    Added the 404 Page

---
+++ client/src/pages/PageNotFound.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import WestIcon from "@mui/icons-material/West";
+
+import { UserContext } from "../context/UserContext";
+
+export default function PageNotFound() {
+  const { user } = useContext(UserContext);
+
+  return (
+    <main className="p-4 md:px-[8rem] md:py-[2rem] flex flex-col gap-2 items-center">
+      <div className="flex gap-8 items-center select-none">
+        <img src="/images/oops.png" alt="" className="h-24 md:h-[8rem]" />
+        <h2 className="text-5xl md:text-[8rem] font-semibold">404</h2>
+      </div>
+      <h4 className="text-3xl md:text-5xl">Page not found!</h4>
+      <p className="text-center">
+        The page you are looking for does not exist or is moved. Try going back
+        to our homepage.
+      </p>
+      <button className="rounded-full bg-amber-600 px-4 py-2 text-sm cursor-pointer opacity-100 hover:opacity-85 w-fit flex items-center gap-2 text-white">
+        <WestIcon />
+        <Link to={user ? "/courses" : "/"}>Homepage</Link>
+      </button>
+    </main>
+  );
+}
