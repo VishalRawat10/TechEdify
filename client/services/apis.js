027e6b8efdb14230887b64cff54f888a6a6365f8
Vishal Rawat <omniverserawat2005@gmail.com>
2025-06-11 15:51:04 +0530

    Divided routes into different modules

--- client/services/apis.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const apiInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}`,
-    withCredentials: true
-});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,70 +1,34 @@
 import { lazy, Suspense } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
-const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
-const Contact = lazy(() => import("./pages/Contact"));
-const Courses = lazy(() => import("./pages/Courses"));
-const ShowCourse = lazy(() => import("./pages/ShowCourse"));
-const Login = lazy(() => import("./pages/Login"));
-const Signup = lazy(() => import("./pages/Signup"));
-const UpdateProfile = lazy(() => import("./pages/UpdateProfile"));
-const Dashboard = lazy(() => import("./pages/Dashboard"));
-const Lectures = lazy(() => import("./pages/Lectures"));
-const ChatBotPage = lazy(() => import("./pages/ChatBotPage"));
-
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 import Loader from "./components/Loader";
-import Header from "./layouts/Header";
-import Footer from "./layouts/Footer";
 
-import AuthWrapper from "./components/AuthWrapper";
 import AllContextProvider from "./context/AllContextProvider";
-import ChatWithAiBtn from "./components/UI/ChatWithAiBtn";
+import { AdminProvider } from "./context/AdminContext";
+import StudentRoutes from "./routes/StudentRoutes";
+import InstructorRoutes from "./routes/InstructorRoutes";
+import AdminRoutes from "./routes/AdminRoutes";
 
 export default function App() {
   return (
     <AllContextProvider>
-      <Header />
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/courses/:id" element={<ShowCourse />} />
+          <Route path={"/*"} element={<StudentRoutes />} />
           <Route
-            path="/courses/:id/learn"
+            path={"/admin/*"}
             element={
-              <AuthWrapper>
-                <Lectures />
-              </AuthWrapper>
+              <AdminProvider>
+                <AdminRoutes />
+              </AdminProvider>
             }
           />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/user/login" element={<Login />} />
-          <Route path="/user/signup" element={<Signup />} />
-          <Route
-            path="/user/dashboard"
-            element={
-              <AuthWrapper>
-                <Dashboard />
-              </AuthWrapper>
-            }
-          />
-          <Route
-            path="/user/update-profile"
-            element={
-              <AuthWrapper>
-                <UpdateProfile />
-              </AuthWrapper>
-            }
-          />
-          <Route path="/chatbot/chat" element={<ChatBotPage />} />
+          <Route path={"/instructor/*"} element={<InstructorRoutes />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Suspense>
-      <ChatWithAiBtn />
-
-      <Footer />
     </AllContextProvider>
   );
 }
