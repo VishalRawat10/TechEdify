6711c735c56250202bf3a39f15692d514ba45671
Vishal Rawat <omniverserawat2005@gmail.com>
2025-06-11 14:24:50 +0530

    Added admin model

---
+++ server/models/admin.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const adminSchema = new Schema({
+    name: String,
+    isTempPassword: {
+        type: Boolean,
+        default: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    comapanyEmail: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        // required : true,
+    }
+});
+
+module.exports = model("admin", adminSchema);
