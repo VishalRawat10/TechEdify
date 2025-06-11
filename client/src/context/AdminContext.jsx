001c2f4c8f259bfe6f727a817aefd86b91431d13
Vishal Rawat <omniverserawat2005@gmail.com>
2025-06-11 14:35:48 +0530

    Added the admin context

---
+++ client/src/context/AdminContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState, useEffect } from "react";
+import { apiInstance } from "../services/apis";
+
+export const AdminContext = createContext();
+export const AdminProvider = ({ children }) => {
+  const [admin, setAdmin] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const getAdmin = async () => {
+    return await apiInstance.get("/admin/verify");
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    getAdmin()
+      .then((res) => setAdmin(res.data.admin))
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <AdminContext.Provider value={{ admin, setAdmin, loading, setLoading }}>
+      {children}
+    </AdminContext.Provider>
+  );
+};
