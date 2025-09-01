f37b4e6552e91ead3ef9a056de9e3c5a858a9077
Vishal Rawat <omniverserawat2005@gmail.com>
2025-09-01 21:55:49 +0530

    Created individual storage for Tutor, Course, User and Lecture

--- server/config/cloudinary.config.js
+++ server/config/cloudinary.config.js
@@ -1,5 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const ExpressError = require('../utils/ExpressError');
 require('dotenv').config();
 
 cloudinary.config({
@@ -8,12 +9,52 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET,
 });
 
-const storage = new CloudinaryStorage({
+const LectureStorage = new CloudinaryStorage({
     cloudinary: cloudinary,
-    params: {
-        folder: "TechEdify",
-        resource_type: "raw"
-    },
+    params: async (req, file) => {
+        const lecNo = req.course.lectures.indexOf(req.params.lectureId) === -1 ? req.course.lectures.length + 1 : req.course.lectures.indexOf(req.params.lectureId) + 1;
+        return {
+            folder: `TechEdify/courses/${req.course._id}/lectures/lecture${lecNo}`,
+            resource_type: "auto",
+            public_id: file.fieldname === "lectureVideo" ? "video" : file.fieldname,
+        }
+    }
 });
 
-module.exports = { cloudinary, storage };
+const CourseStorage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: async (req, file) => {
+        if (!file) {
+            return next(new ExpressError(400, "Thumbnail file is required!"))
+        }
+        return {
+            folder: `TechEdify/courses/${req.params.id}`,
+            resource_type: 'auto',
+            public_id: "thumbnail"
+        }
+    }
+});
+
+const UserStorage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: async (req, file) => {
+        return {
+            folder: `TechEdify/Users/${req.user._id}`,
+            resource_type: 'auto',
+            public_id: "profile-image"
+        }
+    }
+});
+
+const TutorStorage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: async (req, file) => {
+        return {
+            folder: `TechEdify/Tutors/${req.tutor._id}`,
+            resource_type: 'auto',
+            public_id: "profile-image"
+        }
+    }
+});
+
+module.exports = { cloudinary, UserStorage, TutorStorage, LectureStorage, CourseStorage };
