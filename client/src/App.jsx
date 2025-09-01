e4e5bd1c596eb82b16d629f44c4c477bfdb4300e
Vishal Rawat <omniverserawat2005@gmail.com>
2025-09-01 22:33:23 +0530

    changed instructor to tutor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,39 @@
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
-const PageNotFound = lazy(() => import("./pages/PageNotFound"));
-import Loader from "./components/Loader";
+const NotFoundPage = lazy(() => import("./pages/user/NotFoundPage"));
 
 import AllContextProvider from "./context/AllContextProvider";
 import { AdminProvider } from "./context/AdminContext";
 import UserRoutes from "./routes/UserRoutes";
-import InstructorRoutes from "./routes/InstructorRoutes";
+import TutorRoutes from "./routes/TutorRoutes";
 import AdminRoutes from "./routes/AdminRoutes";
+import { TutorProvider } from "./context/TutorContext";
 
 export default function App() {
   return (
     <AllContextProvider>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path={"/*"} element={<UserRoutes />} />
-          <Route
-            path={"/admin/*"}
-            element={
-              <AdminProvider>
-                <AdminRoutes />
-              </AdminProvider>
-            }
-          />
-          <Route path={"/instructor/*"} element={<InstructorRoutes />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path={"/*"} element={<UserRoutes />} />
+        <Route
+          path={"/admin/*"}
+          element={
+            <AdminProvider>
+              <AdminRoutes />
+            </AdminProvider>
+          }
+        />
+        <Route
+          path={"/tutor/*"}
+          element={
+            <TutorProvider>
+              <TutorRoutes />
+            </TutorProvider>
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </AllContextProvider>
   );
 }
