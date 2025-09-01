93ee95637c6856cad5787d4bcc1fe681c9c42616
Vishal Rawat <omniverserawat2005@gmail.com>
2025-09-01 22:04:36 +0530

    Some changes

--- server/middlewares/middlewares.js
+++ server/middlewares/middlewares.js
@@ -6,11 +6,12 @@ const jwt = require("jsonwebtoken");
 const { verifyJwt } = require("../utils/jwtUtils");
 const BlacklistToken = require("../models/blacklistToken");
 const ExpressError = require("../utils/ExpressError");
+const Course = require("../models/course");
 
 module.exports.isAuthenticated = async (req, res, next) => {
     const token = req.signedCookies.token || req.headers.authorization?.split(" ")[1];
     if (!token) {
-        return next(new ExpressError(401, "Unathorized!"));
+        return next(new ExpressError(401, "Unathorized! Auth token missing!"));
     }
     const blackListedToken = await BlacklistToken.findOne({ token });
     if (blackListedToken) {
@@ -35,7 +36,7 @@ module.exports.isAuthenticated = async (req, res, next) => {
 module.exports.authenticateTutor = async (req, res, next) => {
     const tutorToken = req.signedCookies.tutorToken || req.headers.authorization?.split(" ")[1];
     if (!tutorToken) {
-        return next(new ExpressError(401, "Unauthorized!"));
+        return next(new ExpressError(401, "Unauthorized! Auth token missing!"));
     }
     try {
         const blackListedToken = await BlacklistToken.findOne({ token: tutorToken });
@@ -43,14 +44,13 @@ module.exports.authenticateTutor = async (req, res, next) => {
             res.clearCookie("tutorToken", { signed: true });
             return next(new ExpressError(401, "Please login to continue!"));
         }
-
         const decoded = jwt.verify(tutorToken, process.env.TUTOR_JWT_SECRET_KEY);
         const tutor = await Tutor.findById(decoded._id);
         if (!tutor) {
             return next(new ExpressError(401, "Plase login as tutor to continue!"));
         }
         req.tutor = tutor;
-        req.token = token;
+        req.tutorToken = tutorToken;
         return next();
     } catch (err) {
         return next(new ExpressError(500, err.message));
@@ -74,7 +74,7 @@ module.exports.authenticateAdmin = async (req, res, next) => {
             return next(new ExpressError(401, "Admin not found!"));
         }
         req.admin = admin;
-        req.token = token;
+        req.adminToken = adminToken;
         next();
     } catch (err) {
         return next(new ExpressError(500, err.message));
@@ -82,19 +82,30 @@ module.exports.authenticateAdmin = async (req, res, next) => {
 }
 
 module.exports.isEnrolled = async (req, res, next) => {
-    const { courseId } = req.params;
-    if (req.user.coursesEnrolled.includes(courseId)) {
+    const { id } = req.params;
+    const course = await Course.findById(id).select("+enrolledStudents");
+    if (!course) {
+        return next(new ExpressError(400, "Course not found!"));
+    }
+    if (course.enrolledStudents.includes(req.user._id)) {
+        req.course = course;
         return next();
     }
     return next(new ExpressError(401, "You are not enrolled to the course!"));
 }
 
 module.exports.isCourseTutor = async (req, res, next) => {
-    const { courseId } = req.params;
-    if (req.tutor.myCourse.includes(courseId)) {
+    const { id } = req.params;
+    const course = await Course.findById(id).select("+lectures");
+    if (!course) {
+        return next(new ExpressError(400, "Course not found!"));
+    }
+
+    if (course.tutor.toString() === req.tutor._id.toString()) {
+        req.course = course;
         return next();
     }
-    return next(new ExpressError(401, "Unauthorized! You are not the tutor!"));
+    return next(new ExpressError(403, "You are not the tutor of course!"));
 }
 
 module.exports.isReviewAuthor = async (req, res, next) => {
@@ -106,7 +117,7 @@ module.exports.isReviewAuthor = async (req, res, next) => {
     if (review.author === req.user._id) {
         return next();
     }
-    return next(new ExpressError(401, "Unauthorized! You are not the review author!"));
+    return next(new ExpressError(403, "You are not the review author!"));
 }
 
 
