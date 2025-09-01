dc95687a4d50bc6fe990b8eea0a9a9cbaa347f97
Vishal Rawat <omniverserawat2005@gmail.com>
2025-09-01 21:57:41 +0530

    Made some changes

--- server/config/joiSchema.config.js
+++ server/config/joiSchema.config.js
@@ -4,8 +4,8 @@ module.exports.tutorSchema = Joi.object({
     fullname: Joi.string().min(2).required(),
     email: Joi.string().email().required(),
     personalEmail: Joi.string().email().required(),
-    phone: Joi.string().length(10).required(),
     message: Joi.string().min(20).required(),
+    // password: Joi.string().min(8).required()
 });
 
 module.exports.courseSchema = Joi.object({
@@ -13,19 +13,13 @@ module.exports.courseSchema = Joi.object({
     alias: Joi.string().required(),
     description: Joi.string().required(),
     price: Joi.number().min(1).required(),
-    chapters: Joi.string().required()
+    chapters: Joi.string().required(),
+    type: Joi.string().required()
 });
 
 module.exports.lectureSchema = Joi.object({
     title: Joi.string().min(2).required(),
     description: Joi.string().required(),
     status: Joi.string().required(),
-    lectureVideo: {
-        url: Joi.string().required(),
-        filename: Joi.string().required(),
-    },
-    thumbnail: {
-        url: Joi.string().required(),
-        filename: Joi.string().required(),
-    }
+    publicId: Joi.string().required(),
 });
