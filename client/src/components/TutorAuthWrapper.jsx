7686ae35795141bc226572f1c017f1fad2dca8c5
Vishal Rawat <omniverserawat2005@gmail.com>
2025-10-21 13:22:02 +0530

    Added redirect feature

--- client/src/components/TutorAuthWrapper.jsx
+++ client/src/components/TutorAuthWrapper.jsx
@@ -1,18 +1,19 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { TutorContext } from "../context/TutorContext";
 import Loader from "./Loader";
 
 export default function TutorAuthWrapper({ children }) {
   const navigate = useNavigate();
   const { loading, tutor } = useContext(TutorContext);
+  const location = useLocation();
 
   if (loading) {
     return <Loader />;
   }
 
   if (!tutor) {
-    navigate("/tutor/login");
+    navigate(`/tutor/login?redirectTo=${location.pathname}`);
   }
 
   return children;
